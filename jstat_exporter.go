a5c81755249915b0e939693a8c6155c13bc099a2
Alexandre Amor <dev@ralex.fr>
2020-06-05 14:53:28 +0200

    Use promhttp for http handler

--- jstat_exporter.go
+++ jstat_exporter.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/log"
 )
 
@@ -278,7 +279,7 @@ func main() {
 	prometheus.MustRegister(exporter)
 
 	log.Printf("Starting Server: %s", *listenAddress)
-	http.Handle(*metricsPath, prometheus.Handler())
+	http.Handle(*metricsPath, promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`<html>
 		<head><title>jstat Exporter</title></head>
